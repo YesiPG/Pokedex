d7dcefea6d0f42fc0a67c5ceba14e440dec0ec1a
Yesica Perlaza <perlazayesik@gmail.com>
2024-03-05 22:40:56 -0500

    pokemon destacados

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,35 @@
 (function(){
     "use strict";
     const d = document;
-    const carrusel = d.querySelector(".outs_cont2")
-    const tarjeta = d.querySelectorAll(".outs_tarjeta");
-    let index;
+    const carrusel = d.querySelector(".outs_carrusel");
+    const tarjetas = d.querySelectorAll(".outs_cont");
+    let index = 0;
+    let num = 0.491;
 
-    for(let i = 0; i< tarjeta.length; i++){
-        if(tarjeta[i].classList.contains("outs_tarjeta--active")){
-            index = i;
-            break;
-        }
+    const update = ()=>{
+        const offset = - index * (100/num);
+        carrusel.style.transform = `translateX(${offset}px)`;
     }
 
-    console.log(index);
+    
 
     d.addEventListener("click",(e)=>{
-        const tama = carrusel.children.length;
-        let distancia = 200;
-        let nuevoScroll = carrusel.scrollLeft + distancia;
         
         if(e.target.matches(".outs_flechaIzq")){
-            index = (index - 1 + tama) % tama;
-            
-            tarjeta[index+1].classList.remove("outs_tarjeta--active");
-            tarjeta[index].classList.add("outs_tarjeta--active");
-            
-            const nuevoScroll2 = - distancia * index;
-            carrusel.scrollLeft = nuevoScroll2; 
-        }
-        if(e.target.matches(".outs_flechaDer")){
-            index = (index + 1) % tama;
-
-            tarjeta[index-1].classList.remove("outs_tarjeta--active");
-            tarjeta[index].classList.add("outs_tarjeta--active");
-
-            if(nuevoScroll >= carrusel.scrollWidth){
-                nuevoScroll = 0;
+            index--;
+            if (index < 0) {
+                index = 0;
             }
-            carrusel.scrollTo({
-                left: nuevoScroll,
-                behavior: 'smooth'
-            });
+            update();
+        }
+        
+        if(e.target.matches(".outs_flechaDer")){
+            index++;
+            const limiteIndex = tarjetas.length - num;
+            if (index > limiteIndex) {
+                index = limiteIndex;
+            }
+            update();
         }
     })
-})()
+})
